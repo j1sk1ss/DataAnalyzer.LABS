440ccff509e90be61ab92770ab144848c5ab0aa2
j1sk1ss <fotikolay222@gmail.com>
2024-05-06 14:58:55 +0500

    Corr added

--- ui.py
+++ ui.py
@@ -1,6 +1,12 @@
+from collections import Counter
+
 import flet as ft
 import flet_core.control_event
 import pandas as pd
+from flet.matplotlib_chart import MatplotlibChart
+import seaborn as sns
+import pingouin as pg
+from matplotlib import pyplot as plt
 
 from main import load_csv, data_is_loaded, get_data, set_data, summary
 
@@ -98,21 +104,39 @@ def main(page: ft.Page):
 
     # region [Graphs Page]
     # Graphs page ===========
-    def draw_graph(e: flet_core.control_event.ControlEvent):
+
+    min_field = ft.TextField(hint_text="Нижний предел", value="0", label="Минимум")
+    max_field = ft.TextField(hint_text="Верхний предел", value="100000000", label="Максимум")
+
+    def reload_graph_page():
         page_reload()
-        button_headers = [ft.TextButton(x, style=ft.ButtonStyle(color='black'), on_click=draw_graph) for x in get_data().columns]
+        button_headers = [ft.TextButton(x, style=ft.ButtonStyle(color='black'), on_click=draw_default_graph) for x in get_data().columns]
         page.add(
             ft.Row(
-                button_headers
+                [*button_headers, ft.Text("Все данные")]
             )
         )
+
+        button_headers = [ft.TextButton(x, style=ft.ButtonStyle(color='black'), on_click=draw_freq_graph) for x in get_data().columns]
+        page.add(
+            ft.Row(
+                [*button_headers, ft.Text("Частота")]
+            ),
+            ft.Row(
+                [min_field, max_field], alignment=ft.MainAxisAlignment.END
+            )
+        )
+
+    def draw_default_graph(e: flet_core.control_event.ControlEvent):
+        reload_graph_page()
 
         column_name = e.control.text
         column_data = []
 
         column = get_data()[column_name]
         for i in range(len(column)):
-            column_data.append(ft.LineChartDataPoint(i, column[i]))
+            if int(min_field.value) < i < int(max_field.value):
+                column_data.append(ft.LineChartDataPoint(i, column[i]))
 
         chart_data = [ft.LineChartData(
             data_points=column_data,
@@ -122,30 +146,160 @@ def main(page: ft.Page):
             stroke_cap_round=True,
         )]
 
+        bottom_axis = [
+            ft.ChartAxisLabel(
+                value=x,
+                label=ft.Text(str(x), size=10, weight=ft.FontWeight.NORMAL),
+            ) for x in range(len(column_data))
+        ]
+
+        left_axis = [
+            ft.ChartAxisLabel(
+                value=x,
+                label=ft.Text(str(x), size=10, weight=ft.FontWeight.NORMAL),
+            ) for x in range(int(max(get_data()[column_name])), 0, max(int(max(get_data()[column_name]) / 10000), 1))
+        ]
+
         chart = ft.LineChart(
-            data_series=chart_data
+            data_series=chart_data,
+            left_axis=ft.ChartAxis(
+                labels=left_axis
+            ),
+            bottom_axis=ft.ChartAxis(
+                labels=bottom_axis
+            ),
+            border=ft.Border(
+                bottom=ft.BorderSide(4, ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE))
+            )
+        )
+
+        page.add(chart)
+        page.update()
+
+    def draw_freq_graph(e: flet_core.control_event.ControlEvent):
+        reload_graph_page()
+
+        data = get_data()
+        column_name = e.control.text
+        column = data[column_name]
+
+        value_counts = sorted(Counter(column).items(), key=lambda x: x[0])
+
+        column_data = []
+        for value, count in value_counts:
+            if int(min_field.value) < value < int(max_field.value):
+                column_data.append(ft.LineChartDataPoint(value, count))
+
+        chart_data = [ft.LineChartData(
+            data_points=column_data,
+            stroke_width=2,
+            color=ft.colors.LIGHT_GREEN,
+            curved=False,
+            stroke_cap_round=True
+        )]
+
+        bottom_axis = []
+        left_axis = []
+        for value, count in value_counts:
+            bottom_axis.append(
+                ft.ChartAxisLabel(
+                    value=value,
+                    label=ft.Text(str(value), size=10, weight=ft.FontWeight.NORMAL),
+                ))
+
+            left_axis.append(
+                ft.ChartAxisLabel(
+                    value=count,
+                    label=ft.Text(str(count), size=10, weight=ft.FontWeight.NORMAL),
+                ))
+
+        chart = ft.LineChart(
+            data_series=chart_data,
+            left_axis=ft.ChartAxis(
+                labels=left_axis
+            ),
+            bottom_axis=ft.ChartAxis(
+                labels=bottom_axis
+            ),
+            border=ft.Border(
+                bottom=ft.BorderSide(4, ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE))
+            )
         )
 
         page.add(chart)
         page.update()
 
     def open_graphs_page(e):
-        page_reload()
-
-        button_headers = [ft.TextButton(x, style=ft.ButtonStyle(color='black'), on_click=draw_graph) for x in get_data().columns]
-        page.add(
-            ft.Row(
-                button_headers
-            )
-        )
-
+        reload_graph_page()
         page.update()
 
     # endregion
 
+    # region [Corr Page]
     # Corr page ===========
-    def open_corr_page(e):
+
+    def draw_corr(e):
+        reload_corr_page()
+
+        corr_matrix = get_data().corr()
+        fig, ax = plt.subplots()
+        im = ax.imshow(corr_matrix, cmap='hot', interpolation='nearest')
+        fig.colorbar(im)
+
+        ax.set_xticks(range(len(corr_matrix.columns)))
+        ax.set_yticks(range(len(corr_matrix.index)))
+
+        ax.set_xticklabels(corr_matrix.columns)
+        ax.set_yticklabels(corr_matrix.index)
+
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_title('Парная корреляция')
+
+        page.add(MatplotlibChart(fig, expand=True))
+
         page.update()
+
+    def draw_pcorr(e):
+        reload_corr_page()
+
+        pcorr_matrix = get_data().pcorr()
+        fig, ax = plt.subplots()
+        im = ax.imshow(pcorr_matrix, cmap='hot', interpolation='nearest')
+        fig.colorbar(im)
+
+        ax.set_xticks(range(len(pcorr_matrix.columns)))
+        ax.set_yticks(range(len(pcorr_matrix.index)))
+
+        ax.set_xticklabels(pcorr_matrix.columns)
+        ax.set_yticklabels(pcorr_matrix.index)
+
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_title('Частная корреляция')
+
+        page.add(MatplotlibChart(fig, expand=True))
+
+        page.update()
+
+    def reload_corr_page():
+        page_reload()
+        button_headers = [
+            ft.TextButton('Парная корреляция', style=ft.ButtonStyle(color='black'), on_click=draw_corr),
+            ft.TextButton('Частная корреляция', style=ft.ButtonStyle(color='black'), on_click=draw_pcorr)
+        ]
+
+        page.add(
+            ft.Row(
+                [*button_headers]
+            )
+        )
+
+    def open_corr_page(e):
+        reload_corr_page()
+        page.update()
+
+    # endregion
 
     def page_reload():
         page.controls.clear()
@@ -163,16 +317,7 @@ def main(page: ft.Page):
 
         page.update()
 
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.HOME, icon_color='black', on_click=open_main_page),
-                ft.IconButton(ft.icons.SUMMARIZE, icon_color='black', on_click=open_summary_page),
-                ft.IconButton(ft.icons.AUTO_GRAPH, icon_color='black', on_click=open_graphs_page),
-                ft.IconButton(ft.icons.DATA_ARRAY, icon_color='black', on_click=open_corr_page)
-            ],
-        )
-    )
+    page_reload()
 
 
 ft.app(target=main)
